c6fcc2f834fec7877c017d67b7de8f47cce5e1c3
DESKTOP-FTRL8CP\Admin <sukapat.d@kkumail.com>
2018-03-16 21:45:34 +0700

    cake - add template (bootstrap,jquery,popper)-3

--- components/template_css.php
+++ components/template_css.php
@@ -1,2 +1,11 @@
+<!-- mobile settings -->
+    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0" />
+
 <!--Bootstrap CSS-->
-    <link rel="stylesheet" href="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/template/plugins/bootstrap/css/bootstrap.css">
+
+    <!-- THEME CSS -->
+    <link href="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/template/css/essentials.css" rel="stylesheet" type="text/css" />
+    <link href="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/template/css/layout.css" rel="stylesheet" type="text/css" />
+    <link href="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/template/css/color_scheme/green.css" rel="stylesheet" type="text/css" id="color_scheme" />
+
--- components/template_js.php
+++ components/template_js.php
@@ -1,6 +1,7 @@
+<script type="text/javascript">var plugin_path = 'assets/template/plugins/';</script>
 <!--Jquery-->
 <script src="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/jquery/jquery-3.3.1.min.js"></script>
 <!--Popper-->
 <script src="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/popper/popper.js"></script>
-<!--Bootstrap-->
-<script src="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/bootstrap/js/bootstrap.js"></script>
+<!-- JAVASCRIPT FILES -->
+<script type="text/javascript" src="//<?= $_SERVER['SERVER_NAME']?>/narisaclinic/assets/template/js/app.js"></script>
