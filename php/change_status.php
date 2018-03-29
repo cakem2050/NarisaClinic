59a3e369cbfa23d9769bc8638a7be41f546c103f
Nabodee Srivichai <cskku01@gmail.com>
2018-03-29 22:55:13 +0700

    update passcode

--- php/change_status.php
+++ php/change_status.php
@@ -9,9 +9,19 @@
 
 <?php
     include "connect.php";
+
+try{
     $bill = $_POST['bills_id'];
     $stmt = $conn->prepare("UPDATE bills SET bills_status='D' WHERE bills_id=:bills_id");
     $stmt->bindParam(':bills_id', $bill);
     $stmt->execute();
+
+    $stmt_d = $conn->prepare("UPDATE billsdetail SET bild_status='D' WHERE bills_id=:bills_ids");
+    $stmt_d->bindParam(':bills_ids', $bill);
+    $stmt_d->execute();
     echo "pass";
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
 ?>
