7a4f65fd5426a0627a3b7c64dc61b3fa893baedb
DESKTOP-FTRL8CP\Admin <sukapat.d@kkumail.com>
2018-04-02 19:00:08 +0700

    cake - 02/04/2561 : Edit (require) -2

--- assets/js/billlist.js
+++ assets/js/billlist.js
@@ -6,8 +6,7 @@ $(document).ready(function () {
     $('#bill-detail').on('keyup keypress', function(e) {
         var keyCode = e.keyCode || e.which;
         if (keyCode === 13) {
-            // e.preventDefault();
-            // return false;
+            $("#submit").click();
         }
     });
 
